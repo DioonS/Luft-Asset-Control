97bf63e38f661c153cac2e593c2b01aa735cdc38
DioonS <itzdiih@gmail.com>
2023-03-25 17:44:07 -0300

    update in style and add components

--- SpringWeb/src/main/java/com/web/luft/SpringWeb/repository/ColaboradoresRepo.java
+++ SpringWeb/src/main/java/com/web/luft/SpringWeb/repository/ColaboradoresRepo.java
@@ -5,29 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ColaboradoresRepo extends JpaRepository<Colaborador, Integer> {
-
-    //@Query(value = "select count(a) >  from Colaboradores a WHERE a.id = :id")
     boolean existsById(Integer id);
-
-    // Consulta JPQL para maior portabilidade e segurança
-    /*@Query(value = "select CASE WHEN count(a) > 0 THEN 'true' ELSE 'false' END  from Colaboradores a WHERE a.id = :id")
-    boolean existsById(int id);*/
-
-    /*@Query(value = "SELECT a FROM Colaboradores a WHERE a.email = :email AND a.senha = :senha")
-    boolean existsByEmail(String email);
-
-    @Query(value = "SELECT a FROM Colaboradores a WHERE a.email = :email AND a.senha = :senha")
-    Colaboradores findByEmailAndSenha(String email, String senha);
-
-    default Colaboradores login(String email, String senha) {
-        return findByEmailAndSenha(email, senha);
-    }*/
-
-    //Colaborador findByEmail(String email);
-
-    //@Query(value="select * from administradores where email like %:email% or senha like %:senha% ", nativeQuery = true)
-    //public ArrayList<Administrador> findAllByNomeEmail(@Param("email") String email, @Param("senha") String senha);
-
-    //@Query(value="select * from administradores where email = :email and senha = :senha", nativeQuery = true)
-    //public ArrayList<Administrador> findByEmailAndSenha(@Param("email") String email, @Param("senha") String senha);
 }
